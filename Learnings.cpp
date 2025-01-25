b2fc71c462b4c17e5dcecc76e93a8aa5f843f822
Bhav-Codes <bhav8175@gmail.com>
2025-01-26 03:10:02 +0530

    The Second Commit

--- Learnings.cpp
+++ Learnings.cpp
@@ -10,6 +10,7 @@ using namespace std;
 //     }
 //     return b;
 // }
+
 // int main(){
 //     int n;
 //     string temp;
@@ -19,8 +20,10 @@ using namespace std;
 //         for(int i = 0; i < n; i++){
 //             cout << rep(" ",i) <<"File\n";
 //         }
+
 //         cout << "Enter: ";
 //         getline(cin, temp);
+
 //         if(temp == "cd +")
 //             n += 1;
 //         else if(temp == "cd -")
@@ -29,6 +32,7 @@ using namespace std;
 //             cout << "Command not found";
 //     }
 // }
+
 //----------------------------------------------------------------------------------------------------------
 //TESTING VECTORS AND SPLITTING STRINGS
 // int countChr(string s, char splitter){
@@ -40,9 +44,11 @@ using namespace std;
 //     }
 //     return count;
 // }
+
 // vector<string> split(string s, char splitter){
 //     vector<string> arr;
 //     string temp = "";
+
 //     for(int i = 0; i < s.length() ; i++){
 //         if(s[i] == splitter){
 //             arr.push_back(temp);
@@ -53,15 +59,19 @@ using namespace std;
 //     }
 //     return arr;
 // }
+
 // int main(){
 //     string a = "Hello there my name is bhavya singhal";
+
 //     vector<string> b = split(a, ' ');
     
 //     for(int i = 0; i < countChr(a, ' ') + 1; i++){
 //         cout << b[i] << " ";
 //     }
 // }
+
 //------------------------------------------------------------------------------------------------------------------------
+
 // LEARNING MACRO AND TESTING __FILE__ MACRO
 //#include<filesystem> (issue: compiler cannot recognise this library )
 // #include <limits.h> // for getting the current working direcctory
@@ -71,7 +81,9 @@ using namespace std;
 //     string b = __FILE__;
 //     cout << a << b << endl;
 // }
+
 //-----------------------------------------------------------------------------------------------------------------------------------
+
 // LEARNING AND IMPLEMENTING OOPS
 // #include<fstream>
 // class file_op{
@@ -81,6 +93,7 @@ using namespace std;
 //             file.close();
 //         }
 // };
+
 // int main(){
 //     string f;
 //     cout << "Enter file name: ";
@@ -88,26 +101,36 @@ using namespace std;
 //     file_op obj;
 //     obj.create(f);
 // }
+
 //-----------------------------------------------------------------------------------------------------------------------------------
+
 // TRYING TO INCLUDE FILESYSTEM/CSTDIO?
+
 // #include <iostream>
 // #include <cstdio>
 // class file_op{
 // public:
+
 //         void del(string file_path){
+
 //             if(remove(file_path.c_str()) != 0)// c_str used to type cast string to const char*, as remove has paramter as const char*
+
 //                 cout << "No such file or directory as " << file_path << " found."<< endl;
 //         }
 // };
+
 // ---------------------------------------------------------------------------------------------------------------------
-// Including Windows API for getting cwd and opening terminal COPIED FROM AI
+
+// Including Windows API for getting cwd and opening terminal
 // #include<windows.h>
 // void openWin() {
 //     // Command to open a new Command Prompt window
 //     const char* command = "cmd.exe";
+
 //     // Setting up the process information structures
 //     STARTUPINFO si = { sizeof(STARTUPINFO) }; // Startup information structure
 //     PROCESS_INFORMATION pi = {};             // Process information structure
+
 //     // Create a new process (launch terminal)
 //     if (CreateProcess(
 //             nullptr,               // Application name (nullptr allows specifying it in `command`)
@@ -122,6 +145,7 @@ using namespace std;
 //             &pi                    // Process information
 //         )) {
 //         std::cout << "Terminal opened successfully!" << std::endl;
+
 //         // Close process and thread handles
 //         // CloseHandle(pi.hProcess);
 //         // CloseHandle(pi.hThread);
@@ -129,12 +153,56 @@ using namespace std;
 //         std::cerr << "Failed to open terminal. Error: " << GetLastError() << std::endl;
 //     }
 // }
+
 // int main(){
 //     openWin();
 // }
-#include <cstdlib> // For system()
-int main() {
-    string command = "start cmd /c \"g++ program.cpp -o program && program\"";
-    system(command.c_str());
-    return 0;
+
+// #include <windows.h>
+
+// int main() {
+//     char cwd[MAX_PATH];
+//     if (GetCurrentDirectoryA(MAX_PATH, cwd)) {
+//         cout << "Current working directory: " << cwd << std::endl;
+//     } else {
+//         cerr << "Error: Unable to get current working directory." << std::endl;
+//     }
+//     return 0;
+// }
+
+// #include <windows.h>
+
+// int main() {
+//     char cwd[MAX_PATH];
+//     // if (GetCurrentDirectoryA(MAX_PATH, cwd)) {
+//     GetCurrentDirectoryA(MAX_PATH, cwd);
+//     string path = cwd;
+//     cout << path << endl;
+//     return 0;
+// }
+// TRYING CSTDLIB FOR OPENING TERMINAL WINDOW
+
+// #include <cstdlib> // For system()
+// int main() {
+//     string command = "start cmd /c \"g++ program.cpp -o program && program && pause\"";// This command opens a new cmd window, compiles the program, runs it, and then pauses it
+//     // It automatically closes the cmd window after the program has finished running
+//     system(command.c_str());
+//     int a;
+//     cout << "Enter a number: ";
+//     cin >> a;
+//     cout << "Hello";
+    
+//     return 0;
+// }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//TESTING OUT FORMAT
+
+int main(){
+    // cout<<"\t    ======================================================\n";
+	// cout<<"\t   |               FILE MANAGEMENT SYSTEM                 |\n";
+    // cout<<"\t    ======================================================\n\n\n\n\n\n\n\n\n\n\n";
+
+    cout << "FILE MANAGEMENT SYSTEM";
 }
--- file_system.cpp
+++ file_system.cpp
@@ -3,9 +3,13 @@
 #include<string>
 #include<vector> 
 #include<cstdio> // To delete file, Syntax: remove("filename.txt");
-//#include<filesystem> unable to integrate, because old compiler to substituted this - using windows API
-#include<Windows.h> // For getting Current working directory and opening terminal (problem: makes the app windows specific)
+//#include<filesystem> //unable to integrate, because old compiler to substituted this - using windows API
+#include<Windows.h> // For getting Current working directory (problem: makes the app windows specific)(But file system not working because old compiler)
+
+#define APP_NAME "FILE MANAGEMENT SYSTEM"
+
 using namespace std;
+
 // Class of all the basic operations on FILE
 class file_op{
 public:
@@ -13,13 +17,47 @@ public:
             ofstream file(file_path);
             file.close();
         }
+
         void del(string file_path){
+
             if(remove(file_path.c_str()) != 0)// c_str used to type cast string to const char*, as remove has paramter as const char*
-                cout << "No such file or directory as " << file_path << " found."<< endl; 
+
+                cout << "No such file or directory as '" << file_path << "' found."<< endl; 
+        }
+
+        void help(){            
+            cout << "\ncreate: To create a file SYNTAX-> create <fileName/Path>\n"
+                 << "del: To delete a file SYNTAX-> del <fileName/Path>\n"
+                 << "end: To close and Exit the Session\n"
+                 << "\n";
         }
 };
+
+// To validate the number of arguments of a command
+bool validate_args(int arg_inp, int valid_arg, string cmd){
+    if(arg_inp > valid_arg){
+        cout << "Too many arguments for the command '" << cmd << "',\nuse help to display the list and syntax of commands.\n";
+        return false;
+    }
+    else if(arg_inp < valid_arg){
+        cout << "Too few arguments for the command '" << cmd << "',\nuse help to display the list and syntax of commands.\n";
+        return false;
+    }
+    return true;
+}
+
+
+// To get the current working directory
+string getCWD(){
+    char cwd[MAX_PATH]; // An array of char to store path of var, array of 260 char for windows, MAX_PATH is a constn defined in windows.h 
+    GetCurrentDirectoryA(MAX_PATH, cwd);// Function in windows.h  here cwd:A pointer to the buffer where the directory path will be stored
+    string path = cwd;
+    return path;
+}
+
 // Could have gone by displaying the function and assigning a number to them but to make it more natural i've made it linux like
 // Though with small number of commands displaying the list and assigning the number is more handy but as we add more and more commands it starts to clutter
+
 // Used to count the number of splitter
 // Avoided the use of in built count function in algorithm library
 /**
@@ -34,6 +72,7 @@ int countChr(string s, char splitter){
     }
     return count;
 }
+
 // Split function to differentiate between the actual command and the arguments
 /**
  * s: string to be split
@@ -43,6 +82,7 @@ vector<string> split(string s, char splitter){
     s += " ";
     vector<string> arr;
     string temp = "";
+
     for(int i = 0; i < s.length() ; i++){
         if(s[i] == splitter){
             arr.push_back(temp);
@@ -53,36 +93,42 @@ vector<string> split(string s, char splitter){
     }
     return arr;
 }
+
 // Function to display the heirarchy(PENDING...)
-void display_Heirarchy(){
-    // Pending...
-};
+void display_Heirarchy();
+
 // Function to open shell
-void openWin(){ 
-    // Pending...
+void openWin() { // Pending...
 }
+
 int main(){
     vector<string> cmd;
-    cout << "Welcome to file management system\n";
-    
+    cout << "Welcome to " << APP_NAME << "\n";
+    string cwdPath = getCWD();
+
     while(true){
         string cmd;
         vector<string> cmd_arr;
         file_op op;
-        cout << "\nPath Will Be Here> ";
+        bool validity = true; // Flag to check if the command is valid, intoduced this to encounter of printing both invalid arg and cmd not recognised
+
+
+        cout << "\n" << cwdPath << "> ";
         getline(cin, cmd);
         cmd_arr = split(cmd, ' ');
-        if(cmd_arr[0] == "help"){
-            cout << "\ncreate: to create a file SYNTAX-> create <fileName/Path>\n"
-                 << "del: to delete a file SYNTAX-> del <fileName/Path>\n"
-                 << "\n";
-        }
-        else if(cmd_arr[0] == "create")
+
+        if(cmd_arr[0] == "help")
+            op.help();
+        else if(cmd_arr[0] == "create" && (validity = validate_args(cmd_arr.size() - 1, 1, cmd_arr[0])))          
             op.create(cmd_arr[1]);
-        else if(cmd_arr[0] == "del")
+        else if(cmd_arr[0] == "del" && (validity = validate_args(cmd_arr.size() - 1, 1, cmd_arr[0])))
             op.del(cmd_arr[1]);
-        else
-            cout << "Command not found, type help to display list of commands";
+        else if(cmd_arr[0] == "end")
+            break;
+        else if(validity)
+            cout << "Error:'" << cmd_arr[0] << "' is not recognised as a Command,\nuse help for list and of commands.\n";
     }
+    cout << "Thank You for using" << APP_NAME << "\nExiting...\n";
+    
     return 0;
 } 
